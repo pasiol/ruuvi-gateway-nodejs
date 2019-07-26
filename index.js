4fecc9d3571f4475d3955790731c9434b9e84c4c
Pasi <pasi@lpt2.local>
2019-07-26 13:34:42 +0300

    Support for let's encrypt

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const app = require('./app')
+const fs = require('fs')
 const http = require('http')
+const https = require('https')
 const config = require('./utils/config')
 
-const server = http.createServer(app)
+const args = process.argv.slice(2)
+console.log('Getting arguments ', args[0])
+
+let server = null
+
+if (args[0] === 'testmode') {
+  server = http.createServer(app)
+} else {
+  try {
+    var options = {
+      key: fs.readFileSync(
+        `/etc/letsencrypt/archive/${config.DOMAINNAME}/privkey1.pem`
+      ),
+      cert: fs.readFileSync(
+        `/etc/letsencrypt/archive/${config.DOMAINNAME}/fullchain1.pem`
+      ),
+      ca: fs.readFileSync(
+        `/etc/letsencrypt/archive/${config.DOMAINNAME}/chain1.pem`
+      )
+    }
+  } catch (error) {
+    console.log('Not founded ssl-certificates: ', error.code, error.path)
+    process.exit(0)
+  }
+  server = https.createServer(options, app)
+}
 
 server.listen(parseInt(config.PORT), () => {
   console.log(`API-server running on port ${config.PORT}.`)
