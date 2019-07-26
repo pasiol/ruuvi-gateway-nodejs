c8513568b9874150ac720137dddf1b64a35f9ea8
Pasi <pasi@lpt2.local>
2019-07-26 15:53:29 +0300

    adding SSLPATH env

--- utils/config.js
+++ utils/config.js
@@ -2,8 +2,10 @@ require('dotenv').config()
 
 let PORT = process.env.PORT
 let MONGODB_URI = process.env.MONGODB_URI
+let SSLPATH = process.env.SSLPATH
 
 module.exports = {
   MONGODB_URI,
-  PORT
+  PORT,
+  SSLPATH
 }
